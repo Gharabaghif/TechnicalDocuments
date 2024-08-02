17b0e107d5cb727a152b908c4a80e7aed152636c
Gharabaghif <168104641+Gharabaghif@users.noreply.github.com>
2024-08-02 13:16:06 +0330

    Update and rename ActiveDirectoryManagement.ps to ActiveDirectoryManagement.ps1

--- ActiveDirectoryManagement.ps1
+++ ActiveDirectoryManagement.ps1
@@ -1,9 +1,8 @@
-Active Directory Management
+#Active Directory Management
+#Move Inactive Computers for 30 days
 
-
-Move Inactive Computers for 30 days
 import-module activedirectory
-#NEW-ADOrganizationalUnit “Inactive-Computers” –path “OU=HOME-Network,DC=HOME,DC=LAB”
+NEW-ADOrganizationalUnit “Inactive-Computers” –path “OU=HOME-Network,DC=HOME,DC=LAB”
 $inactive = (Get-Date).AddDays(-30)
 $Computers = Get-ADComputer -Property Name,lastLogonDate -Filter {lastLogonDate -lt $inactive}
 $Computers | Set-ADComputer -Description "Computer Disabled on $(Get-Date)"
@@ -11,11 +10,11 @@ $Computers | Set-ADComputer -Enabled $false
 $Computers | Move-ADObject -TargetPath “OU=Inactive-Computers,DC=HOME,DC=LAB”
 ## End Of SCRIPT ##
 
+#To add the date of operation in Description uncomment this line
 # Get-ADcomputer -Filter * -SearchBase "ou=inactive-computers,dc=HOME,dc=LAB" | set-Adcomputer -enabled $false -Description "Computer Disabled on $(Get-Date)"
 
 
-
-Move Inactive Users for 30 days
+#Move Inactive Users for 30 days
 ## This Script has been created by F.Garebaghi ##
 import-module activedirectory
 #NEW-ADOrganizationalUnit “Inactive-Users” –path “OU=HOME-Network,DC=HOME,DC=LAB”
@@ -26,6 +25,5 @@ $Users | Set-ADuser -Enabled $false
 $Users | Move-ADObject -TargetPath “OU=Inactive-Users,DC=HOME,DC=LAB”
 
 ## End Of SCRIPT ##
-
-
+#To disable all of the users in this OU uncomment this line
 ### Get-ADuser -Filter * -SearchBase "ou=inactive-users,dc=HOME,dc=LAB" | set-aduser -enabled $false
